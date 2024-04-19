76f3c33f7f445c0edb25cfacd0d5470417937bb0
cauematheus23 <cauematheus_23@hotmail.com>
2024-04-19 15:35:04 -0300

    Add project files.

---
+++ ExercicioNumeroPrimo/Program.cs
@@ -0,0 +1,23 @@
+﻿int numero = 0,v;
+v = 0;
+do
+{
+    Console.WriteLine("Digite o valor para verificar se ele é primo ou não");
+    numero = int.Parse(Console.ReadLine());
+
+} while (numero <= 0);
+
+for (int i = 1; i <= numero; i++)
+{
+    if ((numero % i) == 0)
+    {
+        v++;
+    }
+} if (v == 2)
+{
+    Console.WriteLine($"{numero} é primo");
+}
+else
+{
+    Console.WriteLine($"{numero} não é primo");
+}
